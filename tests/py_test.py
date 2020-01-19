d708721a7b074b82c05789cbac4c7500cea16edb
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-01-19 22:07:21 +1030

    Updated top level pytest file so it imports the root directory

--- tests/py_test.py
+++ tests/py_test.py
@@ -1,4 +1,12 @@
 import pytest
 
+import os
+import sys
+
+module_dir = os.path.dirname(__file__)
+sys.path.append(os.path.join(module_dir, '../'))
+
+from PoeDevTools.PoeTrade import PoeTrade
+
 def test():
     assert True
