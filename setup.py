56d9fefa8ea6ff4ca290b37ba42e3dc92d267684
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-01-20 17:54:32 +1030

    Adding basic setup.py so it can be installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name = "poedevtools",
+      version = "0.1",
+      description = "A set of tools for helping PoE application developers achieve their goals quicker",
+      url = "https://github.com/CoolDudde4150/poe-devtools",
+      author = "CoolDudde4150",
+      author_email = "CoolDudde4150@gmail.com",
+      packages = find_packages(),
+      include_package_data=True)
