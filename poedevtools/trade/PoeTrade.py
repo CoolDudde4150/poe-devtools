80e5455320e0a18da955d49485d2e7c96d4d9f75
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-01-30 08:05:27 +1030

    hi

--- poedevtools/trade/PoeTrade.py
+++ poedevtools/trade/PoeTrade.py
@@ -1,10 +1,9 @@
-import json
 import requests
-import poedevtools.trade.request_pb2 as PoeRequest
+
 from google.protobuf.json_format import MessageToJson, MessageToDict
-import math
-import pprint
 from google.protobuf import text_format
+import poedevtools.trade.request_pb2 as PoeRequest
+import json
 
 # TODO: Find a way to print all the fields of a protobuf including empty ones.
 # TODO: Add setters and getters for parts of the protobuf
@@ -15,22 +14,25 @@ class PoeTrade:
     search_params = PoeRequest.PoeTradeRequest()
 
     response = {}
-    
+
     def __init__(self):
         # There always needs to be a value for the league
         self.league = "Standard"
 
     def print_query(self):
-    	print(text_format.MessageToString(self.search_params, as_utf8 = True))
+        print(text_format.MessageToString(self.search_params, as_utf8=True))
 
     def get_query(self):
         return self.search_params
 
     def set_query(self, query):
         self.search_params = query
-    
+
     def get_saved_response(self):
         return self.response
+
+    def reset_query(self):
+        self.search_params = PoeRequest.PoeTradeRequest()
 
     # TODO: It would likely be a good idea to return the error received and let the application maker
     #       deal with it.
@@ -42,22 +44,35 @@ class PoeTrade:
     # num_get - The number of results you want returned.
     # save_response - This object can save a search response if wanted for later reference
     # start - A custom search start position. Defaults to 0 if not set
-    def search(self, PoeTradeRequest = None, num_get = 10, save_response = False, start = None):
+    def search(self, PoeTradeRequest=None, num_get=10, save_response=False, start=None):
         # Use the saved trade request if none are provided
-        if PoeTradeRequest != None:
+        if PoeTradeRequest is not None:
             request = PoeTradeRequest
         else:
             request = self.search_params
 
+        # Convert it to a dict to send
+        if isinstance(request, PoeRequest.PoeTradeRequest):
+            json_query = MessageToDict(request, preserving_proto_field_name=True)
+        elif isinstance(request, dict):
+            json_query = request
+        elif isinstance(request, str):
+            json_query = json.loads(request)
+        else:
+            print("Not a valid type for PoeTradeRequest")
+            return None
+
         # Get the id responses from path of exile trade
-        response = requests.post(url = self.search_URL + self.league, json = MessageToDict(request))
+        response = requests.post(
+            url=self.search_URL + self.league,
+            json=json_query,
+        )
 
         dict_response = response.json()
 
-        print(dict_response)
-        # If the query return nothing, return nothing
+        # If the query return nothing, return the error message response. It will still be a json/dict
         if "result" not in dict_response:
-            return None
+            return dict_response
 
         # If there are less results than the requested amount, get all of them.
         if len(dict_response) < num_get:
@@ -67,7 +82,7 @@ class PoeTrade:
         items = {"result": []}
 
         start = 0
-        while (num_get > 0):
+        while num_get > 0:
             # It works but I know there is a cleaner way with modulus >:^()
             if num_get > 10:
                 end = start + 10
@@ -75,21 +90,26 @@ class PoeTrade:
                 end = start + num_get
             num_get -= 10
 
-            fetch_URL_temp = self.fetch_URL
+            fetch_url_temp = self.fetch_URL
             for result in dict_response["result"][start:end]:
-                fetch_URL_temp += result + ","
-            fetch_URL_temp = fetch_URL_temp[:-1] + "?query=" + dict_response["id"]
+                fetch_url_temp += result + ","
+            fetch_url_temp = fetch_url_temp[:-1] + "?query=" + dict_response["id"]
 
-            items_response = requests.get(fetch_URL_temp)
-            for id in items_response.json()["result"]:
-                items["result"].append(id)
+            items_response = requests.get(fetch_url_temp)
+
+            # Try to convert the response to json
+            try:
+                json_item_reponse = items_response.json()
+            except:
+                # Not sure how I want to do this. Return a formatted dict with empty results? Need to see what other errors can occur
+                return items
+
+            for id_ in json_item_reponse["result"]:
+                items["result"].append(id_)
 
             start = end
 
-        if(save_response):
+        if save_response:
             self.response = items
 
         return items
-
-trade = PoeTrade()
-trade.print_query()
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name = "poedevtools",
-      version = "0.1",
-      description = "A set of tools for helping PoE application developers achieve their goals quicker",
-      url = "https://github.com/CoolDudde4150/poe-devtools",
-      author = "CoolDudde4150",
-      author_email = "CoolDudde4150@gmail.com",
-      packages = find_packages(),
-      include_package_data=True)
+setup(
+    name="poedevtools",
+    version="0.1",
+    description="A set of tools for helping PoE application developers achieve their goals quicker",
+    url="https://github.com/CoolDudde4150/poe-devtools",
+    author="CoolDudde4150",
+    author_email="CoolDudde4150@gmail.com",
+    packages=find_packages(exclude=["tests.*"]),
+    py_modules=['poedevtools.trade.PoeTrade'],
+    install_requires=['pytest', 'sphinx', 'protobuf', 'requests', 'numpy'],
+    entry_points={
+        "trade": [
+            "trade = poedevtools.trade.PoeTrade:__init__"
+        ]
+    }
+)
