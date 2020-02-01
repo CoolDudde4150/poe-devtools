5cc68ddc498418df195774377e7c2f868963927e
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-02-01 15:29:19 +1030

    Cant have identically named test files

--- tests/offline/trade/trade_test.py
+++
@@ -1,9 +0,0 @@
-import poedevtools.trade.PoeTrade as PoeTrade
-
-def test_imports():
-    assert True
-
-def test_blank_proto():
-    trade_obj = PoeTrade.PoeTrade()
-
-    assert trade_obj.print_query() == None
