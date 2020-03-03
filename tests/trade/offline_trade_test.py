27801a97228899f823f33de4fb4a7c614b0060f6
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-03-03 10:35:45 +1030

    Fixed for renaming

--- tests/trade/offline_trade_test.py
+++ tests/trade/offline_trade_test.py
@@ -1,18 +1,18 @@
-import poedevtools.trade.PoeTrade as PoeTrade
-import poedevtools.trade.traderequest_pb2 as TradeRequest
+import poedevtools.trade.poetrade as PoeTrade
+import poedevtools.trade.proto.traderequest_pb2 as TradeRequest
 
 def test_imports():
     assert True
 
 def test_blank_proto():
-    trade_obj = PoeTrade.PoeTrade()
+    trade_obj = PoeTrade.PoeTradeAPI()
 
     empty_trade_query = TradeRequest.PoeTradeRequest()
 
     assert trade_obj.query == empty_trade_query
 
 def test_cookie_maker():
-    trade_obj = PoeTrade.PoeTrade()
+    trade_obj = PoeTrade.PoeTradeAPI()
     poesessid = "lolxd"
 
     expected_cookie = {"POESESSID": poesessid}
@@ -20,15 +20,15 @@ def test_cookie_maker():
     assert trade_obj._populate_trade_cookie(poesessid) == expected_cookie
 
 def test_dict_conversion():
-    trade_obj = PoeTrade.PoeTrade()
+    trade_obj = PoeTrade.PoeTradeAPI()
 
     proto_query = trade_obj.query
     dict_query = {}
     json_query = '{"big fake": 123}'
 
-    assert isinstance(PoeTrade.PoeTrade._convert2dict(proto_query), dict)
-    assert isinstance(PoeTrade.PoeTrade._convert2dict(dict_query), dict)
-    assert isinstance(PoeTrade.PoeTrade._convert2dict(json_query), dict)
+    assert isinstance(PoeTrade.PoeTradeAPI._convert2dict(proto_query), dict)
+    assert isinstance(PoeTrade.PoeTradeAPI._convert2dict(dict_query), dict)
+    assert isinstance(PoeTrade.PoeTradeAPI._convert2dict(json_query), dict)
     
 def test_fetch_url_maker():
     results = []
@@ -40,4 +40,4 @@ def test_fetch_url_maker():
         expected_fetch_url += i + ","
     expected_fetch_url = expected_fetch_url[:-1] + "?query=iden"
 
-    assert PoeTrade.PoeTrade._get_fetch_url(results, "iden") == expected_fetch_url
+    assert PoeTrade.PoeTradeAPI._get_fetch_url(results, "iden") == expected_fetch_url
